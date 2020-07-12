bc9d2475943fdf9291bc80d7ad415bc8f12bc5c0
JustATangMan <jasontang127@gmail.com>
2020-07-11 21:15:35 -0400

    jugemu jugmeu

--- testing.java
+++ testing.java
@@ -9,6 +9,8 @@ public class testing {
     public static void main(String[] args) {
         System.out.println("testing");
         System.out.println("nooo you can't just get a perfect score on the ACT! haha 36 go brrrrr");
+        
         System.out.println("triple baby");
+        System.out.println("jugemujugemugokonosurikirekaijarisuigyonosuigyomatsuunraimatsufuraimatsukunerutokoronisumutokoroyaburakojinoburakojipaipopaipopaiponoshuringanshuringannogurindaigurindainoponpokopinoponpokonanochokyuumenochousuke");
     }
 }
